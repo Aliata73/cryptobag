8df1053d2980a67453918da5c069c82bfe80bb30
aliata73 <aliata73@protonmail.com>
2025-02-08 12:33:17 +0330

    Footer Changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import './Footer.css'
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+
 
 function Footer() {
   return (
     <div className='footer'>
         <p>Copyright @ 2025 - CryptoBag - All Right Reserved.</p>
+        <div className='icons'>
+          <a href='https://github.com/Aliata73' target='_blank'>
+          <FaGithub className='icon' />
+          </a>
+          <a href='https://instagram.com/atarod_alii' target='_blank'>
+            <FaInstagram className='icon' />
+          </a>
+        </div>
     </div>
   )
 }
